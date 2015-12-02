07e02843d8be35a138047c087c70ea3340dfc1dc
Samantha Degges <samanthadegges@gmail.com>
2015-12-02 15:58:50 -0800

    first route working.

--- routes/shopping.js
+++ routes/shopping.js
@@ -1,9 +1,11 @@
 "use strict"
 
-// var express = require('express');
-// var router = express.Router();
 var Mongoose = require('mongoose');
 var Schema = Mongoose.Schema;
 var router = require('express').Router()
+
+
+router.get('/', function(req, res, next) {
+  });
 
 module.exports= router;
