ee4ba1541c57b7148d70f2eaab3ae8d3777625d7
Samantha Degges <samanthadegges@gmail.com>
2015-12-02 14:37:36 -0800

    server configured

---
+++ routes/shopping.js
@@ -0,0 +1,9 @@
+"use strict"
+
+// var express = require('express');
+// var router = express.Router();
+var Mongoose = require('mongoose');
+var Schema = Mongoose.Schema;
+var router = require('express').Router()
+
+module.exports= router;
