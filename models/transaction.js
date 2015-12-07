7f716e654396583327569c18ea0f0f1176c8ed89
Samantha Degges <samanthadegges@gmail.com>
2015-12-07 10:20:57 -0800

    removed billing. TBD with stripe integration.

--- models/transaction.js
+++ models/transaction.js
@@ -3,10 +3,11 @@ var Schema = Mongoose.Schema;
 
 var transactionSchema =  Schema({
   submitDate: {type: Date, required: true},
-  purchased: {type: Mongoose.Schema.ObjectId},
+  item: {type: Mongoose.Schema.ObjectId},
   price: {type: Number, required: true},
   images: [{type: Mongoose.Schema.ObjectId}],
-  status: {type: String, default: 'open', enum: ['closed', 'open', 'processing'], required: true}
+  status: {type: String, default: 'open', enum: ['closed', 'open', 'processing'], required: true},
+  customerName: String
 });
 
 var transaction = Mongoose.model('transaction', transactionSchema);
