3c895e9e9c00aea550a306dc596fa3d47a9a1686
Samantha Degges <samanthadegges@gmail.com>
2015-12-07 23:12:46 -0800

    toggling the ng-show() not working from grid div, but works the other way.

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -1,8 +1,30 @@
 var app = angular.module('app');
 
-app.controller('homeCtrl', function($scope, $stateParams, $state) {
-  console.log('home ctrl is working.');
- //$scope.listingsGrid = true; //listing Profile will hide
- $scope.listingProfile= true; //listing Grid will hide. This breaks footer.
+app.controller('homeCtrl', function($scope, $stateParams, $state, listingService) {
   $state.transitionTo('home.listings');
+  $scope.listings;
+  // $scope.check = true;
+
+  // $scope.toggleShow = function(){
+  //   return $scope.check = !$scope.check;
+  //   console.log($scope);
+  // }
+
+  // $scope.hideGrid = true; // why is grid is hiding profile?
+  // $scope.hideProfile = true;
+
+  listingService.getListings().
+  then(function(res) {
+    console.log('res is: ', res.data);
+    $scope.listings = res.data;
+  }, function(err) {
+    alert("error getting listing. Error: "+ err.data);
+    console.log(err.data);
+  })
+
+  console.log('home ctrl is working.');
+
+
+
+
 });
