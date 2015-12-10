3e317f73f741b61980909878cac834e2b3f957d1
Samantha Degges <samanthadegges@gmail.com>
2015-12-09 17:32:26 -0800

    set up dash wireframe accordions

--- public/js/controllers/adminDashCtrl.js
+++ public/js/controllers/adminDashCtrl.js
@@ -1,5 +1,13 @@
 var app = angular.module('app');
 
-app.controller('adminDashCtrl', function($scope, $http) {
-  console.log('adminDashCtrl is working.');
+app.controller('adminDashCtrl', function($scope) {
+  console.log("adminDashCtrl is working.");
+
+  // ACCORDION FUNCTIONALITY
+   $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+   $scope.addItem = function() {
+     var newItemNo = $scope.items.length + 1;
+     $scope.items.push('Item ' + newItemNo);
+   };
 });
