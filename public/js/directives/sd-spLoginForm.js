928a5e7646500c8ab88850b65e2859344888c75f
Samantha Degges <samanthadegges@gmail.com>
2015-12-10 23:01:59 -0800

    SP directive

---
+++ public/js/directives/sd-spLoginForm.js
@@ -0,0 +1,10 @@
+// var app = angular.module('app');
+//
+// app.directive('spLoginForm',function(){
+//   return {
+//     templateUrl: function(tElement,tAttrs){
+//       return tAttrs.templateUrl || 'spLoginForm.tpl.html';
+//     },
+//     controller: 'SpLoginFormCtrl'
+//   };
+// });
