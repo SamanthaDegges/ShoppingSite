fd2927e6b568a4cb953f8f29af2cc22df3a07a03
Samantha Degges <samanthadegges@gmail.com>
2016-01-06 11:59:18 -0800

    single upload works.

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,15 +1,39 @@
 "use strict"
 
-var router = require('express').Router();
 var bodyParser = require('body-parser');
 var multer = require('multer');
-var upload = multer({ dest: 'uploads' });
+var s3 = require('multer-s3');
+var upload = multer({
+  storage: s3({
+    dirname: 'images/product-images',
+    bucket: process.env.S3_BUCKET,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    region: 'us-west-1',
+    filename: function (req, file, cb) {
+      cb(null, Date.now())
+    }
+  })
+});
+var router = require('express').Router();
 
-router.post('/', upload.single('file'), function(req, res, next) {
+router.post('/', upload.single("file"), function(req, res, next) {
   console.log('body is: ', req.body);
   console.log('file is: ', req.file);
   res.send(req.file);
 });
+
+// var upload = multer().single('avatar')
+//
+// router.post('/', function (req, res) {
+//   upload(req, res, function (err) {
+//     if (err) {
+//       console.log('error uploading.', err);
+//       return;
+//     }
+//     // Everything went fine
+//   })
+// });
 
 router.get('/', function(req, res) {
   console.log('get triggered for uploads.');
@@ -18,37 +42,3 @@ router.get('/', function(req, res) {
 });
 
 module.exports = router;
-
-
-
-// app.post('/uploads', upload.fields('itemImages', [
-//   { name: 'featured', maxCount: 1 },
-//   { name: 'gallery', maxCount: 8 }
-// ]), function (req, res, next) {
-//   console.log('singleFile to upload is: ', req.file, 'text body is: ', req.body);
-// });
-
-// var cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
-// app.post('/cool-profile', cpUpload, function (req, res, next) {
-//   // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
-//   //
-//   // e.g.
-//   //  req.files['avatar'][0] -> File
-//   //  req.files['gallery'] -> Array
-//   //
-//   // req.body will contain the text fields, if there were any
-// })
-
-//
-// router.post('/', upload.single('photo'), function(req, res, next) {
-//   Photo.upload(req.file, function(err, photo){
-//     console.log(err || photo);
-//     res.redirect('/#/photos');
-//   });
-// });
-//
-// router.get('/', function(req,res) {
-//   Photo.find({}, function(err, photos) {
-//     res.status(err ? 400 : 200).send(err || photos);
-//   });
-// });
